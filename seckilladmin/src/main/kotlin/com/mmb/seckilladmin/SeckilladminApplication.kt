0b4364efe919c569dd9cb34cf302aa0121552560
clay <Clay@makemoneybuy.cn>
2020-10-30 10:34:12 +0800

    a

---
+++ seckilladmin/src/main/kotlin/com/mmb/seckilladmin/SeckilladminApplication.kt
@@ -0,0 +1,11 @@
+package com.mmb.seckilladmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeckilladminApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeckilladminApplication>(*args)
+}
---
+++ seckilladmin/src/test/kotlin/com/mmb/seckilladmin/SeckilladminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mmb.seckilladmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeckilladminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
